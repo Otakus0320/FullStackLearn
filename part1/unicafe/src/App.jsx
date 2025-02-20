9cd1896ed6727ea5ab1b5b5febef1c877f46c1c4
Otakus0320 <jioboyang@gmail.com>
2025-02-20 08:54:31 +0800

    Finished part1-1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Title = () => {
+    return (
+        <div>
+            <h1>give feedback</h1>
+        </div>
+    )
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const Button = (props) => (
+        <button onClick={props.onClick}>{props.text}</button>
+    )
+
+const Board = (props) => {
+    return (
+        <div>
+            <p>{props.text}</p>
+        </div>
+    )
+}
+
+const TotalBoard = (props) => {
+    return (
+        <div>
+            <h2>statistics</h2>
+            <Board text={props.good} />
+            <Board text={props.neutral} />
+            <Board text={props.bad} />
+
+        </div>
+    )
+}
+
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const setToValueGood = (newValue) => () => {
+        setGood(newValue)
+    }
+
+    const setToValueNeutral = (newValue) => () => {
+        setNeutral(newValue)
+    }
+
+    const setToValueBad = (newValue) => () => {
+        setBad(newValue)
+    }
+
+    return (
+        <div>
+            <Title />
+            <Button onClick={setToValueGood(good+1)} text="good"/>
+            <Button onClick={setToValueNeutral(neutral+1)} text="neutral"/>
+            <Button onClick={setToValueBad(bad+1)} text="bad"/>
+
+            <TotalBoard good={good} neutral={neutral} bad={bad} />
+        </div>
+    )
 }
 
 export default App
