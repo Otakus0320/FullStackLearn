8e7540a8aa5fd9516ecf42613eef8508ee62e52d
Otakus0320 <jioboyang@gmail.com>
2025-02-21 17:53:10 +0800

    Finished part2 - 2.2 & 2.3

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,12 +1,12 @@
 const Course = ({course}) => {
     const name = course.name
     const parts = course.parts
-    console.log(parts);
 
     return (
         <div>
             <h2>{name}</h2>
             {parts.map(part => (<p key={part.id}>{part.name} {part.exercises}</p>))}
+            <p><b>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b></p>
         </div>
     )
 }
