4e710dcefa8704b9252758b38b1641f49a33c117
Otakus0320 <jioboyang@gmail.com>
2025-03-03 19:34:27 +0800

    Finished part6 - 6.14

--- part6/anecdotes-redux/src/main.jsx
+++ part6/anecdotes-redux/src/main.jsx
@@ -1,10 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
-import store from "./utils/store.js";
+import store from "./utils/store.js"
+import anecdotesService from "./services/anecdotes.js"
+import {appendVotes, setVotes} from "./reducers/anecdoteReducer.js"
+
+anecdotesService.getAll().then(response => {
+    store.dispatch(setVotes(response))
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -1,29 +1,10 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: Number((20 * Math.random()).toFixed(0))
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
 
 const voteSlice = createSlice({
   name: "votes",
-  initialState: [...initialState].sort((a, b) => b.votes - a.votes),
+  initialState: [],
   reducers: {
     createVote(state, action) {
       console.log(state)
@@ -39,9 +20,15 @@ const voteSlice = createSlice({
       const voteToChange = state.find(vote => vote.id === id)
       if (voteToChange) voteToChange.votes += 1
       state.sort((a, b) => b.votes - a.votes)
+    },
+    appendVotes(state, action) {
+      state.push(action.payload)
+    },
+    setVotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const {createVote, addVotes} = voteSlice.actions;
+export const {createVote, addVotes, appendVotes, setVotes} = voteSlice.actions;
 export default voteSlice.reducer
