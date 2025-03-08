f52210b316f926ce13106575c2fdfea94bb30072
Otakus0320 <jioboyang@gmail.com>
2025-03-08 21:27:56 +0800

    Finished part7 - 7.1

---
+++ part7/anecdotes-routed/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => (
+    <div>
+        Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
+
+        See <a
+        href='https://github.com/Otakus0320/FullStackLearn'>https://github.com/Otakus0320/FullStackLearn</a> for
+        the source code.
+    </div>
+)
+export default Footer
