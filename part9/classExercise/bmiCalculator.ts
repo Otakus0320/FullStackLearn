06b3dd0d3712ddf0a5e229f7014a34b790742661
Otakus0320 <jioboyang@gmail.com>
2025-03-12 16:42:46 +0800

    Finished part9 - 9.6

--- part9/classExercise/bmiCalculator.ts
+++ part9/classExercise/bmiCalculator.ts
@@ -1,14 +1,14 @@
 const calculateBmi = (height: number, weight: number): string => {
     height /= 100;
-    let bmi: number = weight / (height * height)
+    const bmi: number = weight / (height * height);
     if (bmi >= 18.5 || bmi <= 24){
-        return "Normal Range"
+        return "Normal Range";
     }else if (bmi >= 24){
-        return "Overweight"
+        return "Overweight";
     }else {
-        return "Over Light"
+        return "Over Light";
     }
-}
+};
 
 const height: number = Number(process.argv[2]);
 const weight: number = Number(process.argv[3]);
--- part9/classExercise/exerciseCalculator.ts
+++ part9/classExercise/exerciseCalculator.ts
@@ -9,22 +9,22 @@ interface Result {
 }
 
 const calculateExercises = (time: Array<number>, target: number): Result => {
-    let sum: number = time.reduce((acc: number, currentValue: number) => acc + currentValue, 0);
-    let trainingDays: number = time.reduce((acc: number, currentValue:number) => acc + (currentValue ? 1 : 0), 0);
-    let length: number = time.length;
-    let avg: number = sum / length;
-    let success: boolean = avg >= target;
+    const sum: number = time.reduce((acc: number, currentValue: number) => acc + currentValue, 0);
+    const trainingDays: number = time.reduce((acc: number, currentValue:number) => acc + (currentValue ? 1 : 0), 0);
+    const length: number = time.length;
+    const avg: number = sum / length;
+    const success: boolean = avg >= target;
     let rating: number;
     let ratingDescription: string;
     if (sum <= target) {
         rating = 0;
-        ratingDescription = "Keep going"
+        ratingDescription = "Keep going";
     }else if(sum == target) {
         rating = 1;
-        ratingDescription = "Success"
+        ratingDescription = "Success";
     }else{
         rating = 2;
-        ratingDescription = "Nice job"
+        ratingDescription = "Nice job";
     }
     return {
         periodLength: length,
@@ -34,8 +34,8 @@ const calculateExercises = (time: Array<number>, target: number): Result => {
         success: success,
         rating: rating,
         ratingDescription: ratingDescription
-    }
-}
+    };
+};
 
 const args = process.argv.slice(2);
 const time: Array<number> = args.slice(1).map(arg => Number(arg));
